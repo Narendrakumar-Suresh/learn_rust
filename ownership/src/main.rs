8784fec62a8d80b3e4b77b87f539fcccc05acb16
Narendrakumar-Suresh <narendrkumarsuresh@gmail.com>
2026-02-27 17:25:19 +0530

    ownership is weird in practise

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    let mut balance:u32=100;
+
+    add_money(&mut balance, 300);
+    take_money(&mut balance, 30);
+    view_balance(&balance);
+
+}
+
+fn add_money(balance:&mut u32,amt:u32){
+    // * is used to get the value from the reference var
+    // then we return the value.
+    *balance+=amt
+}
+
+fn take_money(balance:&mut u32,amt:u32){
+    // * is used to get the value from the reference var
+    // then we return the value.
+    if amt > *balance{
+        println!("Sorry not enough balance, withdrawl failed!");
+        return
+    }
+    *balance-=amt;
+}
+
+
+fn view_balance(balance:&u32){
+    // we dont use mut ptr beocz it is read
+    println!("{balance}");
 }
